39fec5a2c70eaaca96c553266baf641aa8055e6b
KPZaraGit <kpzara@gmail.com>
2023-01-17 20:55:40 +0800

    Move test data used in contact-page spec file to data\contacts.js file

---
+++ cypress/data/contacts.js
@@ -0,0 +1,4 @@
+export const CONTACT_1 = {
+    name: 'Kevin Tester',
+    email: 'kevin.tester@testit.com' 
+}
--- cypress/e2e/contact-page.cy.js
+++ cypress/e2e/contact-page.cy.js
@@ -1,3 +1,8 @@
+import {CONTACT_1} from "../data/contacts"
+
+const contact1 = CONTACT_1
+const message = 'This is a looooooooong text in message.'
+
 describe("Visit contact page", () => {
   beforeEach(function () {
     //1. From the home page go to contact page
@@ -5,44 +10,43 @@ describe("Visit contact page", () => {
     cy.get('#nav-contact').click()
   })
 
-    it("Test Case 1 - error message test", () => {
+  it("Test Case 1 - error message test", () => {
 
-      //2. Click submit button
-      cy.get('.btn-contact').click()
+    //2. Click submit button
+    cy.get('.btn-contact').click()
 
-      //3. Verify error messages
-      cy.get('.alert-error').should('be.visible').contains('but we won\'t get it unless you complete the form correctly.')
-      cy.get('#forename-err').should('be.visible').should('have.text','Forename is required')
-      cy.get('#email-err').should('be.visible').should('have.text','Email is required')
-      cy.get('#message-err').should('be.visible').should('have.text','Message is required')
+    //3. Verify error messages
+    cy.get('.alert-error').should('be.visible').contains('but we won\'t get it unless you complete the form correctly.')
+    cy.get('#forename-err').should('be.visible').should('have.text','Forename is required')
+    cy.get('#email-err').should('be.visible').should('have.text','Email is required')
+    cy.get('#message-err').should('be.visible').should('have.text','Message is required')
 
-      //4. Populate mandatory fields
-      cy.get('#forename').type('Kevin Tester')
-      cy.get('#email').type('kevin+tester@testit.com')
-      cy.get('#message').type('This is a looooooooong text in message.')
+    //4. Populate mandatory fields
+    cy.get('#forename').type(contact1.name)
+    cy.get('#email').type(contact1.email)
+    cy.get('#message').type(message)
 
-      //5. Validate errors are gone
-      cy.get('#forename-err').should('not.exist')
-      cy.get('#email-err').should('not.exist')
-      cy.get('#message-err').should('not.exist')
+    //5. Validate errors are gone
+    cy.get('#forename-err').should('not.exist')
+    cy.get('#email-err').should('not.exist')
+    cy.get('#message-err').should('not.exist')
 
-    })
+  })
 
-    it("Test Case 2 - successful submission message test", () => {
+  it("Test Case 2 - successful submission message test", () => {
 
-      //2. Populate mandatory fields
-      cy.get('#forename').type('Kevin Tester')
-      cy.get('#email').type('kevin+tester@testit.com')
-      cy.get('#message').type('This is a looooooooong text in message.')
+    //2. Populate mandatory fields
+    cy.get('#forename').type(contact1.name)
+    cy.get('#email').type(contact1.email)
+    cy.get('#message').type(message)
 
-      //3. Click submit button
-      cy.get('.btn-contact').click()
-      
-      cy.get('.popup.modal').should('be.visible')
-      
-      //4. Validate successful submission message
-      cy.get('.alert-success').should('be.visible')
-                              .should('contain','Thanks Kevin Tester, we appreciate your feedback.')
-
-    })  
+    //3. Click submit button
+    cy.get('.btn-contact').click()
+    
+    cy.get('.popup.modal').should('be.visible')
+    
+    //4. Validate successful submission message
+    cy.get('.alert-success').should('be.visible')
+                            .should('contain','Thanks Kevin Tester, we appreciate your feedback.')
+  })  
 })
